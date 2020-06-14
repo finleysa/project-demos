40ca3dddfb361e1e32d6cb7de6e10b9aa6fc6227
finleysa <steve.a.finley@gmail.com>
2020-06-14 17:34:56 -0500

    changed sorted timer

--- src/app/services/snack.service.ts
+++ src/app/services/snack.service.ts
@@ -9,8 +9,8 @@ export class SnackService {
   constructor(private snackBar: MatSnackBar, private router: Router) {}
 
   sorted(msTime: number) {
-    if (!msTime || msTime === 0) { return; }
     const seconds = Math.floor(msTime / 1000);
+    if (!msTime || msTime <= 0) { return; }
     this.snackBar.open(`Array sorted in ${seconds} seconds`, null, {
       duration: 3000
     });
